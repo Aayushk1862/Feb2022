ea1838401a95a8fec85bedf629e25758edc1d24c
AAYUSH KUMAR <aayush.209303085@muj.manipal.edu>
2022-02-25 13:01:07 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; //test case variable
+    cin>>t;
+    while(t!=0)
+    {
+        int a,b;
+        cin>>a>>b;
+        int result = a+b;
+        cout<<result;
+        t=t-1;
+    }
+return 0;
+}
